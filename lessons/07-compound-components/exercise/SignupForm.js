0428dd5fef310ad17f4f55947d5589bf48239e57
Chris Spalding <cspalding93@gmail.com>
2019-08-21 10:47:37 -0600

    exercise 7

--- lessons/07-compound-components/exercise/SignupForm.js
+++ lessons/07-compound-components/exercise/SignupForm.js
@@ -58,12 +58,11 @@ export default function SignupForm() {
         <TextInput id="password" label="Password" />
         <p>
           <span>Start:</span>{" "}
-          <DateFields
-            value={startDate}
-            onChange={setStartDate}
-            start={2018}
-            end={2019}
-          />
+          <DateFields onChange={setStartDate} date={startDate}>
+            <DayField aria-label="Start Day"/>
+            <MonthField aria-label="Start Month"/>
+            <YearField start={2018} end={2019}/>
+          </DateFields>
         </p>
         <TabsButton>
           <FaDumbbell />
