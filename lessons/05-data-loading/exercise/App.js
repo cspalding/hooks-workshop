6c7d7a7fc72ec2d9aea10784e179bdaf2baddf12
Chris Spalding <cspalding93@gmail.com>
2019-08-20 21:55:25 -0600

    exercise 5

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -3,10 +3,26 @@ import React, { useState, useEffect } from 'react'
 import { onAuthStateChanged } from 'app/utils'
 import LoggedIn from 'app/LoggedIn'
 import LoggedOut from 'app/LoggedOut'
+// import { setFlagsFromString } from 'v8';
+
+function useAuth() {
+  const [auth, setAuth] = useState(null)
+  const [authAttempted, setAuthAttempted] = useState(false)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth => {
+      setAuth(auth)
+      setAuthAttempted(true)
+    })
+
+    return unsubscribe
+  }, [])
+
+  return {auth, authAttempted}
+}
 
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const { auth, authAttempted } = useAuth()
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
